2a855a9312773d439bc5982e74241cc8a757c7fc
Roland <rolandmartialkuate@gmail.com>
2023-06-18 23:42:05 +0100

    Add image on producteurs

--- dashboard/producteurs/index.php
+++ dashboard/producteurs/index.php
@@ -158,16 +158,21 @@
     <script src="../../vendor/jquery/jquery-3.2.1.min.js"></script>
     <script src="../../vendor/bootstrap/js/bootstrap.min.js"></script>
     <script>
+        function toggleModal() {
+            $('.container-custom-modal').toggleClass('open')
+        }
         add_producteur();
         delete_producteur();
         function delete_producteur() {
             $('.delete-producteur').click(function () {
                 var id = $(this).attr('id');
-
-                $.post('traitements/delete-producteur.php', {producteur_id: id}, function () {
-                    setTimeout(function(){// wait for 5 secs(2)
-                        location.reload();
-                    }, 2000);
+                toggleModal();
+                $('.confirm-delete').click(function () {
+                    $.post('traitements/delete-producteur.php', {producteur_id: id}, function () {
+                        setTimeout(function(){// wait for 5 secs(2)
+                            location.reload();
+                        }, 2000);
+                    })
                 })
             })
         }
