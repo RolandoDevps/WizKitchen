b805d2d7c97dfa2a2713ff55bf3de1b57f765ece
Roland <rolandmartialkuate@gmail.com>
2023-06-22 12:05:37 +0100

    Render HomePage dynamiq

--- view/index.php
+++ view/index.php
@@ -1,49 +1,37 @@
-<?php require "includes/header.php"; ?>
+<?php
+require "includes/header.php";
+?>
 
 <section class="section_item">
   <div class="fond-5"></div>
   <div class="container">
     <div class="content_item">
-      <div class="custo-card card-atelier row">
-        <div class="card_img col-sm-12 col-md-6">
-          <img src="assets/images/items/1.png" alt="">
-        </div>
-        <div class="card_description col-md-6 col-sm-12">
-          <h2>Votre plat maison sera bien plus réconfortant qu’un plat commandé !</h2>
-          <p>Plus sain, et plus fier, vous serez en dégustant vos plats, élaborés par vous-même, avec des ingrédients frais et respectueux de votre corps ainsi que de la planète.
-          </p>
-          <div class="container_btn_link">
-            <a href="#" class="btn_link color_marron">Découvrir</a>
+    <?php
+        require "includes/connect.php";
+        $response = $bdd->query('SELECT * FROM db_wizkitchen.ateliers ORDER BY id DESC LIMIT 4');
+        $i = 0;
+        // On affiche chaque entrée une à une
+        while ($atelier = $response->fetch()) {
+            $i +=1 ;
+        ?>
+          <div class="custo-card card-atelier row mt-5 <?php if($i % 2 == 0) echo "inverse" ?>">
+            <div class="card_img col-sm-12 col-md-6">
+              <img src="../uploads/<?php echo $atelier['image_url']; ?>" height="100" width="100" alt="img"/>
+            </div>
+            <div class="card_description col-md-6 col-sm-12">
+              <h2><?php echo $atelier['label']; ?></h2>
+              <p>
+                  <?php echo $atelier['description']; ?>
+              </p>
+              <div class="container_btn_link">
+                <a href="#" class="btn_link color_marron">Découvrir</a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-
-      <div class="custo-card card-atelier row inverse mt-11">
-        <div class="card_img col-md-6 col-sm-12">
-          <img src="assets/images/items/2.png" alt="">
-        </div>
-        <div class="card_description col-md-6 col-sm-12">
-          <h2>Mangez sain au quotidien devient simple et motivant !</h2>
-          <p>Manger sain ne vous demandera plus des heures de préparation et de réflexion sur ce que vous pouvez ou ne pouvez pas manger. On ne vous prépare que des plats équilibrés avec des ingrédients riches en nutriments pour une alimentation saine et simple!</p>
-          <div class="container_btn_link">
-            <a href="#" class="btn_link color_marron">Découvrir</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="custo-card card-atelier row mt-11">
-        <div class="card_img col-md-6 col-sm-12">
-          <img src="assets/images/items/3.png" alt="">
-        </div>
-        <div class="card_description col-md-6 col-sm-12">
-          <h2 class="font-37">Plus on est de fous, plus on rit !</h2>
-          <p>La cuisine c’est bien, mais en groupe c’est mieux ! Retrouvez-vous autour d’un plan de travail pour rire et apprendre tout en gagnant du temps sur votre vie perso. </p>
-          <div class="container_btn_link">
-            <a href="#" class="btn_link color_marron">Découvrir</a>
-          </div>
-        </div>
-      </div>
-
+        <?php
+            }
+        $response->closeCursor(); // Termine le traitement de la requête
+        ?>
     </div>
   </div>
 </section>
@@ -54,39 +42,18 @@
   <div class="fond-3"></div>
   <div class="container">
     <div class="content_item">
-
-      <div class="custo-card no_image mt-48">
-        <div class="card_description">
+      <div class="custo-card no_image mt-12 mb-12">
+      <div class="card_img col-sm-12 col-md-6">
+          <img src="../uploads/96634311.png" height="100" width="100" alt="img"/>
+      </div>
+        <div class="card_description col-sm-12 col-md-6">
           <h2 class="color_f9ede6">Nos producteurs</h2>
           <p class="color_fffbf9">L’équipe Wiz s'implique au maximum pour respecter les 2 choses les plus importantes à nos yeux, votre corps et notre planète ! C’est pourquoi nous collaborons avec de petits producteurs qui proposent des produits bio, des associations ou organismes locaux qui souhaitent faire évoluer les habitudes de consommation avec nous.</p>
           <div class="container_btn_link">
-            <a href="#" class="btn_link color_e4c9b9">Découvrir</a>
+            <a href="../nos-producteurs.php" class="btn_link color_e4c9b9">Découvrir</a>
           </div>
         </div>
       </div>
-
-    </div>
-  </div>
-</section>
-
-<section class="section_item relative">
-  <div class="fond-6"></div>
-  <div class="container">
-    <div class="content_item">
-
-      <div class="custo-card card-atelier row inverse mt-35">
-        <div class="card_description col-sm-12 col-md-6">
-          <h2>Pas d’inspiration pour vos plats ?</h2>
-          <p>Cessez de vous prendre la tête à construire des listes de courses en essayant de ne rien oublier, nous pensons et rassemblons tous les produits nécessaires pour vous…</p>
-          <div class="container_btn_link">
-            <a href="#" class="btn_link color_marron">Découvrir</a>
-          </div>
-        </div>
-        <div class="card_img col-sm-12 col-md-6">
-          <img src="assets/images/items/4.png" alt="">
-        </div>
-      </div>
-
     </div>
   </div>
 </section>
@@ -96,65 +63,39 @@
     <h1 class="titlesection">Vos avis</h1>
     <div class="content_item_avis">
 
-      <div class="card_avis">
-        <div class="card_avis_top">
-          <div class="card_img_avis">
-            <img src="assets/images/items/5.png" alt="">
-          </div>
-          <p>Maggie L</p>
-        </div>
-        <div class="card_star">
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star no-color"></i>
-        </div>
-        <div class="card_description_avis">
-          <h2>Pas d’iUn gain de temps</h2>
-          <p>"Le batch-cooking a complètement transformé ma routine culinaire. Des repas savoureux, une organisation simplifiée et un gain de temps précieux. Je ne peux plus m'en passer!"</p>
-        </div>
-      </div>
-
-      <div class="card_avis">
-        <div class="card_avis_top">
-          <div class="card_img_avis">
-            <img src="assets/images/items/6.png" alt="">
-          </div>
-          <p>William G</p>
-        </div>
-        <div class="card_star">
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-        </div>
-        <div class="card_description_avis">
-          <h2>Des plats équilibrés</h2>
-          <p>Grâce au batch-cooking proposé par WIZKITCHEN, je redécouvre le plaisir de manger sainement sans passer des heures en cuisine. Une solution pratique et délicieuse pour des repas équilibrés au quotidien.</p>
-        </div>
-      </div>
-
-      <div class="card_avis">
-        <div class="card_avis_top">
-          <div class="card_img_avis">
-            <img src="assets/images/items/7.png" alt="">
-          </div>
-          <p>Jeanne T</p>
-        </div>
-        <div class="card_star">
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star no-color"></i>
-        </div>
-        <div class="card_description_avis">
-          <h2>Économique et écologique !</h2>
-          <p>Les services de batch-cooking de WIZKITCHEN ont révolutionné ma façon de cuisiner. Des plats délicieux prêts en un clin d'œil, parfait pour les journées chargées!</p>
-        </div>
-      </div>
+        <?php
+            require "includes/connect.php";
+            $response = $bdd->query('SELECT * FROM db_wizkitchen.avis ORDER BY id DESC LIMIT 3');
+            // On affiche chaque entrée une à une
+            while ($avis= $response->fetch()) {
+        ?>
+              <div class="card_avis">
+                <div class="card_avis_top">
+                  <div class="card_img_avis">
+                      <img src="../uploads/<?php echo $avis['image_url']; ?>" height="100" width="100"
+                           alt="img"/>
+                  </div>
+                  <p><?php echo $avis['author']; ?></p>
+                </div>
+                <div class="card_star">
+                    <?php
+                        for($i = 0; $i < $avis['rating'] -1; $i++){
+                    ?>
+                        <i class="fas fa-star"></i>
+                    <?php
+                        }
+                    ?>
+                  <i class="fas fa-star no-color"></i>
+                </div>
+                <div class="card_description_avis">
+                  <h2><?php echo $avis['subject']; ?></h2>
+                  <p><?php echo $avis['content']; ?></p>
+                </div>
+              </div>
+                <?php
+            }
+        $response->closeCursor(); // Termine le traitement de la requête
+        ?>
 
     </div>
   </div>
